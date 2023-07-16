05f1ebd93b0f509e27e7ca30a8543e512b535b3a
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-07-16 19:05:00 +0330

    Add tests in seperate folder

--- tests/test_managers.py
+++ tests/test_managers.py
@@ -58,16 +58,5 @@ class WebSocketTests(unittest.TestCase):
         self.assertFalse(self.manager.active_connections)
 
 
-# class WebSocketEndpointTests(unittest.TestCase):
-#     @patch("main.websocket_endpoint")
-#     def test_websocket_endpoint(self, websocket_endpoint_mock):
-#         # Mock the websocket_endpoint function
-#         websocket_endpoint_mock.return_value = None
-
-#         client = TestClient(app)
-#         with client.websocket_connect("/ws") as websocket:
-#             # Perform necessary assertions or interactions with the websocket
-#             pass
-
 if __name__ == "__main__":
     unittest.main()
