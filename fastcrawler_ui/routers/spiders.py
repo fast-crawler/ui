4b1423b8fbd45b869af76f47b99f01873e95cef5
Sadegh Yazdani <silverstar10@gmail.com>
2023-08-01 14:51:58 +0330

    📝 Update docstring

--- fastcrawler_ui/routers/spiders.py
+++ fastcrawler_ui/routers/spiders.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastcrawler import FastCrawler
+from fastcrawler.schedule.schema import Task
 
 from fastcrawler_ui.controllers.spider import CrawlerController
 from fastcrawler_ui.core.fastapi.depends import get_crawler
@@ -8,23 +9,19 @@ from fastcrawler_ui.repository.spiders import CrawlerRepository
 router = APIRouter()
 crawler_repository = CrawlerRepository()
 
-
 # @router.get("/all")
 # async def clients(crawler: FastCrawler = Depends(get_crawler)):
 #     results = await crawler.controller.all()
 #     return [result.model_dump() for result in results]
 
 
-@router.get("/all")
+@router.get("/all")  # , response_model=list[Task])
 async def clients(crawler: FastCrawler = Depends(get_crawler)):
     """
     clients endpoint for retrieves crawler tasks.
 
-    Args:
-        crawler (FastCrawler): The crawler instance.
-
-    Returns:
-        list[dict[str, Any]]
+    \f
+    :param items: list of Task.
     """
-
-    return await CrawlerController(crawler_repository).get_task(crawler)
+    items = await CrawlerController(crawler_repository).get_task(crawler)
+    return items
