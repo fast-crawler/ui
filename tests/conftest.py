82f6db8b4d9d5a1607e45cc960284ab03f282bad
Sadegh Yazdani <silverstar10@gmail.com>
2023-08-14 08:49:29 +0330

    ✅ Improve spider test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,29 @@
 # pylint: skip-file
-
+import os
+import sys
+import asyncio
 import pytest_asyncio
 
 from fastcrawler.engine.contracts import RequestCycle
-from fastcrawler import BaseModel, Depends, FastCrawler as _FastCrawler, Process, Spider, XPATHField
+from fastcrawler import (
+    BaseModel,
+    Depends,
+    FastCrawler as _FastCrawler,
+    Process,
+    Spider,
+    XPATHField,
+)
+
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from fastapi.testclient import TestClient
+from fastcrawler_ui.core.fastapi.app import app
+from fastcrawler_ui.core.fastapi.sync import sync_crawler_to_fastapi
 
 started_crawler_flag = False
 
-stopted_crawler_flag = 0
+stopped_crawler_flag = 0
 
 total_crawled = 0
 
@@ -51,21 +67,21 @@ class MySpiderStarted(Spider):
         assert started_crawler_flag == True
 
 
-class MySpiderStoped(Spider):
+class MySpiderStopped(Spider):
     engine_request_limit = 5
     data_model = PersonPage
     start_url = Depends(get_urls)
 
     async def save_cycle(self, all_data: list[RequestCycle]) -> None:
-        global stopted_crawler_flag
-        stopted_crawler_flag += 1
+        global stopped_crawler_flag
+        stopped_crawler_flag += 1
         return await super().save_cycle(all_data)
 
     async def save(self, all_data: list[PersonPage]):
-        global stopted_crawler_flag
+        global stopped_crawler_flag
         assert all_data is not None
         assert len(all_data) == 10
-        assert stopted_crawler_flag == 1
+        assert stopped_crawler_flag == 1
 
 
 class MySpider(Spider):
@@ -92,11 +108,11 @@ def get_started_fastcrawler():
     return crawler
 
 
-def get_stopted_fastcrawler():
+def get_stopped_fastcrawler():
     crawler = FastCrawler(
         crawlers=[
             Process(
-                spider=MySpiderStoped(),
+                spider=MySpiderStopped(),
                 cond="every 5 minute",
             ),
         ]
@@ -112,8 +128,8 @@ async def started_crawler():
 
 
 @pytest_asyncio.fixture(scope="function")
-async def stopted_crawler():
-    crawler = get_stopted_fastcrawler()
+async def stopped_crawler():
+    crawler = get_stopped_fastcrawler()
     # await run_async(crawler=crawler, uvicorn_config={"port": 8001})
     for process in crawler.crawlers:
         await process.stop()
@@ -130,4 +146,11 @@ def get_fastcrawler():
     return crawler
 
 
-
+@pytest_asyncio.fixture(scope="session")
+def client():
+    crawler = get_fastcrawler()
+    sync_crawler_to_fastapi(app, crawler)
+    client = TestClient(app)
+    asyncio.run(crawler.run2())
+    
+    yield client
--- tests/test_spider.py
+++ tests/test_spider.py
@@ -3,27 +3,13 @@ import sys
 import asyncio
 
 import pytest
-from fastapi.testclient import TestClient
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from fastcrawler_ui.core.fastapi.app import app
-from fastcrawler_ui.core.fastapi.sync import sync_crawler_to_fastapi
-from .conftest import get_fastcrawler
+from tests.conftest import client
 
 
-crawler = get_fastcrawler()
-
-sync_crawler_to_fastapi(app, crawler)
-
-client = TestClient(app)
-
-asyncio.run(crawler.run2())
 
 
-def test_all():
+def test_all(client):
     response = client.get("/all")
-    print(response)
     content = response.json()
     assert response.status_code == 200
     assert len(content) == 1
