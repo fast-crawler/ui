4acc74104a0d7bcdc98e25745ea4a5c5f07f5707
Sadegh Yazdani <silverstar10@gmail.com>
2023-08-17 15:37:47 +0330

    ✅ Add and update spider tests

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -39,50 +39,75 @@ def test_stop_task(client: TestClient):
 
 def test_start_task(client: TestClient):
     task_name = get_exist_task_name(client)
-    task_original = get_task_by_name(client, task_name)
+    task_before_start = get_task_by_name(client, task_name)
     response = client.post(f"/start_task?task_name={task_name}")
-    task_started = get_task_by_name(client, task_name)
-    content = response.text
+    task_after_start = get_task_by_name(client, task_name)
     assert response.status_code == 204
-    assert content == ""
-    assert task_original["name"] == task_started["name"]
-    assert task_started["disabled"] == (not task_original["disabled"]) is False
+    assert task_before_start["name"] == task_after_start["name"]
+    assert task_after_start["disabled"] == (not task_before_start["disabled"]) is False
 
 
 def test_task_invalid(client: TestClient):
     response = client.post("/start_task?task_name=SHOULD_NOT_FIND")
-    assert response.status_code != 200
+    assert response.status_code == 400
 
 
 def test_toggle_task(client: TestClient):
     task_name = get_exist_task_name(client)
-    task_original = get_task_by_name(client, task_name)
+    task_before_toggle = get_task_by_name(client, task_name)
     response = client.post(f"/toggle_task?task_name={task_name}")
-    task_toggled = get_task_by_name(client, task_name)
-    content = response.text
+    task_after_toggle = get_task_by_name(client, task_name)
     assert response.status_code == 204
-    assert content == ""
-    assert task_original["name"] == task_toggled["name"]
-    assert task_original["disabled"] == (not task_toggled["disabled"])
+    assert task_before_toggle["name"] == task_after_toggle["name"]
+    assert task_before_toggle["disabled"] == (not task_after_toggle["disabled"])
 
 
-def test_update_task(client: TestClient):
-    task_name = get_exist_task_name(client)
-    original_task_setting = get_task_by_name(client, task_name)
-    data = {
+def test_update_task_invalid_name(client: TestClient):
+    task_name = "INVALID_TASK_NAME_SHOULD_NOT_FIND"
+    task_setting_for_update = {
         "description": "sample Description",
     }
 
     response = client.post(
-        f"/update_task?task_name={task_name}", data=json.dumps(data)  # type: ignore
+        f"/update_task?task_name={task_name}", data=json.dumps(task_setting_for_update)  # type: ignore
     )
-    content = response.json()
-    assert response.status_code == 200
-    assert len(content) == 11
-    for key in original_task_setting.keys():
-        if key not in data:
-            assert content[key] == original_task_setting[key]
+    task_response_update = response.json()
+    assert task_response_update["detail"] == f"Task '{task_name}' not found"
+    assert response.status_code == 400
 
-    assert content["description"] == data["description"]
-    task_setting = get_task_by_name(client, task_name)
-    assert task_setting["description"] == data["description"]
+
+def test_update_task(client: TestClient):
+    task_name = get_exist_task_name(client)
+    task_before_update = get_task_by_name(client, task_name)
+    task_setting_for_update = {
+        "description": "sample Description",
+    }
+
+    response = client.post(
+        f"/update_task?task_name={task_name}", data=json.dumps(task_setting_for_update)  # type: ignore
+    )
+    task_response_update = response.json()
+    assert response.status_code == 200
+    assert len(task_response_update) == 11
+    for key in task_before_update.keys():
+        if key not in task_setting_for_update:
+            assert task_response_update[key] == task_before_update[key]
+
+    assert task_response_update["description"] == task_setting_for_update["description"]
+    task_after_update = get_task_by_name(client, task_name)
+    assert task_after_update["description"] == task_setting_for_update["description"]
+
+
+def test_change_task_schedule(client: TestClient):
+    task_name = get_exist_task_name(client)
+    task_before_change_schedule = get_task_by_name(client, task_name)
+    new_task_schedule = "every 52 minute"
+
+    response = client.post(
+        f"/change_task_schedule?task_name={task_name}&task_schedule={new_task_schedule}",
+    )
+    assert response.status_code == 200
+
+    task_after_change_schedule = response.json()
+    assert task_before_change_schedule["start_cond"] != task_after_change_schedule["start_cond"]
+    assert task_after_change_schedule["start_cond"] == new_task_schedule
