198f4ea49ee7fe3d83565e159a8ea7e26fa7ccc1
ManiMozaffar <mani.mozaffar@gmail.com>
2023-09-12 17:49:57 +0300

    ♻️ Refactor ws code

--- fastcrawler_ui/controllers/ws.py
+++ fastcrawler_ui/controllers/ws.py
@@ -1,18 +1,19 @@
-import asyncio
+import json
 
-from fastapi import Depends, WebSocket, WebSocketDisconnect
+from fastapi import Depends, WebSocket
 
-from fastcrawler_ui.repository.ws import ConnectionRepository
+from fastcrawler_ui.repository.ws import ConnectionRepository, Message
 
 
 class WSController:
     def __init__(self, ws_connection_repo: ConnectionRepository = Depends(ConnectionRepository)):
         self.ws_connection_repo = ws_connection_repo
 
-    async def get_connection(self, websocket: WebSocket):
-        await self.ws_connection_repo.connect(websocket)
-        try:
+    async def get_and_send_message(self, websocket: WebSocket):
+        async with self.ws_connection_repo.open_connection(websocket) as connection:
             while True:
-                await asyncio.sleep(1)
-        except WebSocketDisconnect:
-            self.ws_connection_repo.disconnect(websocket)
+                print("HEY!")
+                data = await websocket.receive_text()
+                data_obj = Message.model_construct(**json.loads(data))
+                connection.messages.append(data_obj)
+                await connection.broadcast(exclude_connection={websocket})
