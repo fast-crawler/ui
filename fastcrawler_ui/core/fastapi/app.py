8f4fdbd36b35346981b51a48e7cf160cce114121
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-09-30 21:57:20 +0330

    ✨ allow all origins for now

--- fastcrawler_ui/core/fastapi/app.py
+++ fastcrawler_ui/core/fastapi/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import get_routers
 
 
 def get_application(router=get_routers(), base_app=FastAPI) -> FastAPI:
     app = base_app()
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(router)
     return app
 
