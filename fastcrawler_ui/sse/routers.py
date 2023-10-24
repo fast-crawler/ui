c3c38684c692b5fe2c015e1ee957d525ad2e2f65
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-10-24 21:02:02 +0330

    ❌Delete starletee sse

--- fastcrawler_ui/sse/routers.py
+++
@@ -1,8 +0,0 @@
-from starlette.routing import Route
-from controller import logs, chart
-
-
-routes = [
-    Route("/{crawler_id}/logs", endpoint=logs),
-    Route("/{crawler_id}/chart", endpoint=chart),
-]
--- fastcrawler_ui/sse/run.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from starlette.applications import Starlette
-from routers import routes
-
-app = Starlette(debug=True, routes=routes)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level='info')
