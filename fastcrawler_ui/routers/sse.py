cbd02d325f4964a25747bcf0315edb9950a0f545
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-11-10 11:39:08 +0330

    😋 Add endpoint for list, uuid for each crawler

--- fastcrawler_ui/routers/sse.py
+++ fastcrawler_ui/routers/sse.py
@@ -1,6 +1,7 @@
 from fastapi.responses import StreamingResponse
 from fastapi import APIRouter
 
+from uuid import uuid4
 import datetime
 import asyncio
 import random
@@ -41,7 +42,7 @@ async def generate_random_chart():
                 }
             }
         yield json.dumps(data)
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(1)
 
 
 async def generate_random_crawler_data():
@@ -62,26 +63,48 @@ async def generate_random_crawler_data():
 
 
 async def generate_random_crawlers_data():
+    uuids = [str(uuid4()) for _ in range(3)]
     names = ('Digikala', 'DevTo', 'Medium')
     started_at = tuple(datetime.datetime.utcnow().isoformat() for i in range(3))
     durations = ('12:23', '11:55', '03:19')
     states = ['Active', 'Pause', 'Finished']
     while True:
-        iterator = zip(names, started_at, durations, states)
+        iterator = zip(uuids, names, started_at, durations, states)
         data = {
             'data': [{
+                'id': id_,
                 'name': d1,
                 'started_at': d2,
                 'duration': d3,
                 'state': d4
-                } for d1, d2, d3, d4 in iterator]
+                } for id_, d1, d2, d3, d4 in iterator]
             }
         yield json.dumps(data)
         await asyncio.sleep(0.5)
 
 
+async def generate_random_crawler_detail():
+    uuid = str(uuid4())
+    started_at = datetime.datetime.utcnow().isoformat()
+    while True:
+        data = {
+            'data': [{
+                'id': uuid,
+                'name': 'Digikala',
+                'started_at': started_at,
+                'duration': '12:23',
+                'state': 'Active',
+                }]
+            }
+        yield json.dumps(data)
+        await asyncio.sleep(1)
 
 router = APIRouter()
+
+@router.get("/{crawler_uuid}/detail")
+async def crawler_detail(crawler_uuid: str):
+    return StreamingResponse(content=generate_random_crawler_detail(),
+                             media_type='application/x-ndjson')
 
 @router.get("/{crawler_uuid}/chart")
 async def crawler_chart(crawler_uuid: str):
