4eb8fc1fd022e82e93223bbd0e5e45c5b43c950a
Sadegh Yazdani <silverstar10@gmail.com>
2023-11-17 20:27:26 +0330

    ✨ Serve frontend in backend

--- fastcrawler_ui/core/fastapi/app.py
+++ fastcrawler_ui/core/fastapi/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import get_routers
 
@@ -6,6 +7,7 @@ from .routers import get_routers
 def get_application(router=get_routers(), base_app=FastAPI) -> FastAPI:
     app = base_app()
     app.include_router(router)
+    app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
     return app
 
 
