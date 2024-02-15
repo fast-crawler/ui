688fdb877aa51cd60f8f7e45d9d3051d010d7a8c
Mohammad Sadegh Majidi Kadkani <74909796+Cmatrix1@users.noreply.github.com>
2024-02-15 13:11:55 +0330

    ✨ allow all origins for now (#12)
    Co-authored-by: amirbahador <55255442+amirbahador-hub@users.noreply.github.com>

--- fastcrawler_ui/core/fastapi/app.py
+++ fastcrawler_ui/core/fastapi/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from .routers import get_routers
@@ -6,6 +7,14 @@ from .routers import get_routers
 
 def get_application(router=get_routers(), base_app=FastAPI) -> FastAPI:
     app = base_app()
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(router)
     app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
     return app
