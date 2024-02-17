0571595cbfc1bcc7d4d169074683c2041300a691
Sadegh Yazdani <silverstar10@gmail.com>
2024-02-17 08:18:48 +0330

    🐛 Fix relative path of dist on frontend

--- fastcrawler_ui/core/fastapi/app.py
+++ fastcrawler_ui/core/fastapi/app.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from .routers import get_routers
 
+dist_frontend = Path(__file__).parent.parent.parent.parent / "frontend" / "dist"
 
 def get_application(router=get_routers(), base_app=FastAPI) -> FastAPI:
     app = base_app()
@@ -16,7 +19,7 @@ def get_application(router=get_routers(), base_app=FastAPI) -> FastAPI:
         allow_headers=["*"],
     )
     app.include_router(router)
-    app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
+    app.mount("/", StaticFiles(directory=dist_frontend, html=True), name="static")
     return app
 
 
